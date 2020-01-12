c0895d7a2cf6f0bcc5af73ee764fd6bf73771f70
rohit <rohitkumarjain36@gmail.com>
2020-01-12 10:31:50 +0530

    Adding java-simple package

---
+++ java_simple/src/main/java/adobe_package/App.java
@@ -0,0 +1,14 @@
+package adobe_package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Deployed Successfully !" );
+    }
+}
---
+++ java_simple/src/test/java/adobe_package/AppTest.java
@@ -0,0 +1,20 @@
+package adobe_package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
